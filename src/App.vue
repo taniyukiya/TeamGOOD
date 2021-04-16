598cc8d788e1f991a73216d0c9b9776adf885367
HarunobuTsukamoto <tsukamotohrnb@sohobb.jp>
2021-04-16 18:31:43 +0900

    add async func

--- src/App.vue
+++ src/App.vue
@@ -2,73 +2,155 @@
   <div>
     <div id="outpuArea">
       <myheader></myheader>
-      <textarea v-model="msg"></textarea>
-      <p id="outputHTML" v-html="topicMsg + topicNum + '<br/>' + msg.replace(/\n/g,'<br/>')"></p>
+      <!-- <textarea id="outputTextArea" v-model="titleText"></textarea> -->
+      <ul>
+        <li v-for="item in displayItems" :key="item.title">
+          {{ item.title }}
+        </li>
+      </ul>
+      <input type="range" min="0" max="11" v-model="range" />
+      <!-- <p id="outputHTML" v-html="titleText.replace(/\n/g,'<br/>')"></p> -->
     </div>
     <div id="inputArea">
-      <input id="TopickNumBox" type="text" v-model="topicNum">
-      <input id="TextBox" type="text" v-model="inputText">
-      <button id="Button" @click="addText(), addTopicNum()">click here</button>
+      <input id="TopickNumBox" type="text" v-model="topicNum" />
+      <input id="TextBox" type="text" v-model="inputText" />
+      <button id="Button" @click="asyncAddText()">click here</button>
+      <!-- <button id="FetchButton" @click="created()">fetch</button> -->
     </div>
   </div>
 </template>
 
 <script>
-import myheader from "./components/myheader"
+import myheader from "./components/myheader";
 
-export default{
+export default {
   components: {
-    myheader
+    myheader,
   },
-  data(){
+  data() {
     return {
       msg: "input text is apear here!\n",
       topicMsg: "TopicNum: ",
-      topicNum: 0,
-      inputText: ""
-    }
+      topicNum: 10,
+      inputText: "",
+      titleText: "",
+      titleList: [],
+      linkList: [],
+      dateList: [],
+      items: [],
+      range: 10,
+    };
+  },
+  computed: {
+    displayItems: function () {
+      return this.items.slice(0, this.range);
+    },
+  },
+  created() {
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ topic: "WORLD", lang: "jpn" }),
+    };
+    // fetch("http://localhost:4000/list/topic", requestOptions)
+    //   .then((response) => response.json())
+    //   .then((json) => this.getNewsTitleList(json))
+    //   .catch((err) => console.log(err));
+    fetch("http://localhost:4000/list/topic", requestOptions)
+      .then((response) => response.json())
+      .then((items) => (this.items = items));
   },
   methods: {
-    clear(){
-      this.msg = ""
+    clear() {
+      this.msg = "";
     },
-    addText(){
-      console.log("click");   
-      if (this.inputText !== ""){
-        this.msg += "TopicNum: " + this.topicNum + "///" + this.inputText + "\n";
-      }else{
+    addText() {
+      console.log("click");
+      if (this.inputText !== "") {
+        this.msg +=
+          "TopicNum: " + this.topicNum + "///" + this.inputText + "\n";
+        this.titleText = "";
+      } else {
         this.msg += "textbox is empty \n";
       }
+      console.log("titleList:" + this.titleList.length);
+      for (var i = 0; i < this.topicNum; i++) {
+        this.titleText += this.dateList[i] + "\n";
+        this.titleText += this.titleList[i] + "\n";
+        this.titleText += this.linkList[i] + "\n\n";
+      }
     },
-    addTopicNum(){
-      
-    }
-  }  
-}
+    promiseGetJson() {
+      const jsonObj = new Promise(function (resolve) {
+        const requestOptions = {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ topic: "WORLD", lang: "jpn" }),
+        };
+        fetch(
+          "http://localhost:4000/list/topic",
+          requestOptions
+        ).then((response) => resolve(response.json()));
+      });
+      return jsonObj;
+    },
+    promiseGetInfoFromJson(jsonObj) {
+      const jsonInfo = new Promise(function (resolve) {
+        var titleList = [];
+        jsonObj.forEach((obj) => titleList.push(obj["title"]));
+        resolve(titleList);
+      });
+      return jsonInfo;
+    },
+    async asyncAddText() {
+      var jsonObj = await this.promiseGetJson();
+      var title = await this.promiseGetInfoFromJson(jsonObj);
+      this.titleList = title;
+
+      this.titleText = "";
+      for (var i = 0; i < this.topicNum; i++) {
+        this.titleText += this.titleList[i] + "\n\n";
+      }
+    },
+
+    getNewsTitleList(obj) {
+      obj.forEach((jsonObj) => this.pushList(jsonObj));
+    },
+    pushList(obj) {
+      this.titleList.push(obj["title"]);
+      this.linkList.push(obj["link"]);
+      this.dateList.push(obj["isoDate"]);
+    },
+  },
+};
 </script>
 
 <style>
-  #inputArea{
-    position: fixed;
-    align-items: center;
-    bottom: 0;
-  }
-  #outputArea{
-    display: block;
-    flex-direction: colomn;
-    align-items: left
-  }
-  #TextBox{
-    width: 80vw;
-  }
-  textarea{
-    position: absolute;
-    height: 40vh;
-    width: 80vw;
-  }
-  #outputHTML{
-    position: absolute;
-    bottom: 0;
-    height: 50vh;
-  }
+#inputArea {
+  position: fixed;
+  align-items: center;
+  bottom: 0;
+}
+#outputArea {
+  display: block;
+  flex-direction: colomn;
+  align-items: left;
+}
+#outputTextArea {
+  resize: none;
+  position: absolute;
+  height: 70vh;
+  width: 80vw;
+}
+#TextBox {
+  width: 80vw;
+}
+#outputHTML {
+  position: absolute;
+  height: 50vh;
+}
 </style>
--- src/components/myheader.vue
+++ src/components/myheader.vue
@@ -1,5 +1,5 @@
 <template>
     <div>
-        This is text writer!
+        GOOGLE NEWS
     </div>
 </template>
