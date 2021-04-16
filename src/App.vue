69f416b9b5064186052f12bcb1c68748457a8659
taniyukiya <taniyky@sohobb.jp>
2021-04-16 18:35:28 +0900

    vue.tani

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,30 @@
 <template>
   <div>
-     <myheader></myheader>
-     <news/>
-    <p v-if="msg.length>0">
-      {{msg}}
-    </p>
-    <p v-else>Please input more than 1 length</p>
-    <input id="TextBox" type="text" v-model="msg">
-    <button @click="clear()">clear</button>
-    <!-- <button @click="created()">create</button> -->
+    <myheader></myheader>
+    <news v-bind:msg="WORLD"></news>
   </div>
 </template>
 
-
-
 <script>
-import myheader from './components/myheader'
-import news from './components/news'
+import myheader from "./components/myheader";
+import news from "./components/news";
 
-export default{
-  components:{
-    myheader
+export default {
+  components: {
+    myheader,
+    news,
   },
-  data(){
+  data() {
     return {
-      msg: "Hello World!"
-    }
-  },
-  methods:{
-    clear(){
-      this.msg = ''
-    }
-  },
-  created(){
-
-    const requestOptions = {
-    method:"POST",
-     headers:{"Content-Type":"application/json"},
-     body:JSON.stringify({topic: "WORLD",lang:"jpn",before:2010/8/30})
+      msg: "WORLD",
     };
-        //https://news.google.com/news/rss/headlines/section/topic/WORLD直接とってくるのはセキュリティ上出来ないのでサーバーをかませる。
-    fetch('http://localhost:4000/list/topic',requestOptions)
-    .then(response => {
-      return response.json()
-    })
-    .then( data => {
-    this.msg = data;
-    console.log(data)
-    })
-    .catch( (err) => {
-      console.log(err);
-    //this.msg = err
-    });
-  }
-}
+  },
+};
 </script>
 
 <style>
-  #TextBox{
-    position: fixed;
-    top: 100px;
-  }
+#TextBox {
+  position: fixed;
+  top: 100px;
+}
 </style>
--- src/components/news.vue
+++ src/components/news.vue
@@ -1,53 +1,49 @@
 <template>
   <div>
-    <p v-if="msg.length>0">
-      {{msg}}
+    <p>
+      {{ article }}
     </p>
-    <p v-else>Please input more than 1 length</p>
-    <input id="TextBox" type="text" v-model="msg">
-    <button @click="clear()">clear</button>
-    <!-- <button @click="created()">create</button> -->
+    <!-- <button @click="clear()">clear</button> -->
   </div>
 </template>
 
-
-
 <script>
-
-export default{
-
-    name: 'news',
-    props:{
-        msg: String
-    },
-
-  methods:{
-    clear(){
-      this.msg = ''
-    }
-  },
-  created(){
-
-    const requestOptions = {
-    method:"POST",
-     headers:{"Content-Type":"application/json"},
-     body:JSON.stringify({topic: "WORLD",lang:"jpn",before:2010/8/30})
+let selectTopic = "WORLD";
+export default {
+  data() {
+    return {
+      article: "loading",
     };
-        //https://news.google.com/news/rss/headlines/section/topic/WORLD直接とってくるのはセキュリティ上出来ないのでサーバーをかませる。
-    fetch('http://localhost:4000/list/topic',requestOptions)
-    .then(response => {
-      return response.json()
-    })
-    .then( data => {
-    this.msg = data;
-    console.log(data)
-    })
-    .catch( (err) => {
-      console.log(err);
-    //this.msg = err
-    });
-  }
-}
+  },
+  name: "news",
+  props: {
+    msg: { type: String },
+  },
+  created() {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        topic: selectTopic,
+        lang: "jpn",
+        before: 2010 / 8 / 30,
+      }),
+    };
+    //https://news.google.com/news/rss/headlines/section/topic/WORLD直接とってくるのはセキュリティ上出来ないのでサーバーをかませる。
+    fetch("http://localhost:4000/list/topic", requestOptions)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        this.article = data;
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+        //this.msg = err
+      });
+  },
+};
 // const GoogleNewsRss = require('google-news-rss');
 // const googleNews = new GoogleNewsRss();
 // googleNews
@@ -56,8 +52,8 @@ export default{
 </script>
 
 <style>
-  #TextBox{
-    position: fixed;
-    top: 100px;
-  }
+#TextBox {
+  position: fixed;
+  top: 100px;
+}
 </style>
