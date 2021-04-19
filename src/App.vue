f86fdcd1f25df474f9f892d7fa6b76d2739d50d1
taniyukiya <taniyky@sohobb.jp>
2021-04-19 13:29:20 +0900

    sort

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
-    <div id="outpuArea">
+    <div id="outputArea">
       <myheader></myheader>
       <ul>
-        <li v-for="item in displayItems" :key="item">
-          {{ item.pubDate }}<br>
+        <li v-for="item in displayItems" v-bind:key="item.title">
+          {{ item.pubDate }}<br />
           <a v-bind:href="item.link">{{ item.title }}</a>
         </li>
       </ul>
     </div>
     <div id="inputArea">
-      <input type="range" min="0" max="11" v-model="range">
+      <input type="range" min="0" max="10" v-model="range" />
+      <button @click="sortBy('title')">title</button>
+      <button @click="sortBy('isoDate')">isoDate</button>
+      <p>"現在のソート："{{ sort_key }}:{{ sort_asc ? "昇順" : "降順" }}</p>
     </div>
   </div>
 </template>
@@ -26,11 +29,19 @@ export default {
     return {
       items: [],
       range: 5,
+      sort_key: "",
+      sort_name:"",
+      sort_asc: true,
     };
   },
   computed: {
-    displayItems: function () {
+    displayItems: function() {
+      this.sort_items();
       return this.items.slice(0, this.range);
+    },
+
+    reversItems() {
+      return this.items.slice().reverse();
     },
   },
   created() {
@@ -44,9 +55,28 @@ export default {
     fetch("http://localhost:4000/list/topic", requestOptions)
       .then((response) => response.json())
       .then((items) => (this.items = items));
+    //.then((items)=>(items.isoDate = items.isoDate.replace('-','/')));
   },
   methods: {
-    
+    sort_items() {
+      console.debug("sort_items", this.sort_key);
+      if (this.sort_key === "") {
+        return;
+      }
+      let set = 1;
+      this.sort_asc ? (set = 1) : (set = -1);
+      this.items.sort((a, b) => {
+        if (a[this.sort_key] < b[this.sort_key]) return -1 * set;
+        if (a[this.sort_key] > b[this.sort_key]) return 1 * set;
+        return 0;
+      });
+    },
+    sortBy(key) {
+      this.sort_key === key
+        ? (this.sort_asc = !this.sort_asc)
+        : (this.sort_asc = true);
+      this.sort_key = key;
+    },
   },
 };
 </script>
