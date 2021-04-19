e4c5588c39a1ef982e4a93112e606e5c0f56ea92
Shamno <yuina@mbm.nifty.com>
2021-04-19 17:39:45 +0900

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
+    <myheader></myheader>
+    
     <div id="outputArea">
-      <myheader></myheader>
-      <ul>
-        <p>TOPIC:{{ topicName }}</p>
-        <li v-for="item in displayItems" :key="item.title">
-          {{ item.pubDate }}<br />
-          <a v-bind:href="item.link">{{ item.title }}</a>
-        </li>
-      </ul>
+            <ul>
+              <li v-for="item in displayItems" :key="item.title">
+                {{ item.pubDate }}<br />
+                <a v-bind:href="item.link">{{ item.title }}</a>
+              </li>
+              <p>TOPIC:{{ topicName }}</p>
+            </ul>
     </div>
     <div id="inputArea">
       <input type="range" min="0" max="11" v-model="range" />
@@ -16,10 +17,15 @@
       <pulldownMenuLang :topicLang.sync="topicLang"></pulldownMenuLang>
       <button @click="sortBy('title')">title</button>
       <button @click="sortBy('isoDate')">isoDate</button>
-      <p>"現在のソート："{{ sort_key }}:{{ sort_asc ? "昇順" : "降順" }}</p>
+      <p>現在のソート：{{ sort_key }}{{ sort_asc ? "昇順" : "降順" }}</p>
     </div>
   </div>
 </template>
+
+
+
+
+
 
 <script>
 import myheader from "./components/myheader";
@@ -117,15 +123,45 @@ export default {
 </script>
 
 <style>
+
+*{
+  padding: 0;
+  margin:0;
+}
+
+body{
+  
+  background-color: #CEE5F2;
+}
+
+button {
+  display: inline-block;
+  padding: 0.5em 1em;
+  text-decoration: none;
+  background: #f7f7f7;
+  border-left: solid 6px #ff7c5c;/*左線*/
+  color: #ff7c5c;/*文字色*/
+  font-weight: bold;
+  box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.29);
+}
+button:active {
+  box-shadow: inset 0 0 2px rgba(128, 128, 128, 0.1);
+  transform: translateY(2px);
+}
+
 #inputArea {
   position: fixed;
   align-items: center;
-  bottom: 0;
+  bottom: 5px;
+  width: 100%;
+  padding: 10px;
+  background-color: #ACCBE1;
 }
 #outputArea {
   display: block;
   flex-direction: colomn;
   align-items: left;
+  margin:5%;
 }
 #outputTextArea {
   resize: none;
@@ -133,4 +169,32 @@ export default {
   height: 70vh;
   width: 80vw;
 }
+
+
+/*フリー素材*/
+ul, ol {
+  padding: 0;
+  position: relative;
+}
+
+ul li, ol li {
+  color: #2d8fdd;
+  border-left: solid 6px #2d8fdd;/*左側の線*/
+  background: #f1f8ff;/*背景色*/
+  margin-bottom: 3px;/*下のバーとの余白*/
+   line-height: 1.5;
+  padding: 0.5em;
+  list-style-type: none!important;/*ポチ消す*/
+}
+
+li a{
+  color:#007A74;
+  text-decoration:none;
+}
+
+li a:hover{
+  opacity: 0.5;
+  transition: 0.3s;
+}
+
 </style>
--- src/components/myheader.vue
+++ src/components/myheader.vue
@@ -1,5 +1,36 @@
 <template>
-    <div>
-        ニュースサイトTeamGOOD
+    <div id="header">
+            <p class="title" id="main_title">Team.Good</p>
+            <p class="title">～グッドなニュースサイト～</p>
     </div>
-</template>>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c&family=Pacifico&display=swap');
+
+#header{
+    font-family: 'Pacifico', 'M PLUS Rounded 1c';
+    color: #694A38;
+    background:linear-gradient(to bottom right, #F4AC45 60%,#ff80bb);
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    z-index: 10;
+}
+
+.title{
+    float: left;
+    margin:30px;
+}
+
+#main_title{
+    font-size: 40px;
+    margin:5px;
+}
+
+h1{
+    margin:10px;
+}
+
+</style>
